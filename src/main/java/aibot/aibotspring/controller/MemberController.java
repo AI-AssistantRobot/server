95f4f88e553c1d452a181bdb3eb14fe1fd70c21c
Jsplix <jkjin112@gmail.com>
2024-01-18 01:49:34 +0900

    update commit (2024.01.18)

--- src/main/java/aibot/aibotspring/controller/MemberController.java
+++ src/main/java/aibot/aibotspring/controller/MemberController.java
@@ -1,27 +1,29 @@
 package aibot.aibotspring.controller;
 
+import aibot.aibotspring.dto.MemberDTO;
+import aibot.aibotspring.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
-    @GetMapping("/member/register")
-    public String registerForm() {
-        return "register";
+
+    private final MemberService memberService;
+    @GetMapping("/member/save")
+    public String saveForm() {
+        return "save";
     }
 
-    @PostMapping("/member/register")
-    public String register(@RequestParam("memberId") String memberId,
-                           @RequestParam("memberPassword") String memberPassword,
-                           @RequestParam("memberName") String memberName,
-                           @RequestParam("memberPhone") String memberPhone,
-                           @RequestParam("memberEmail") String memberEmail,
-                           @RequestParam("memberLibrary") String memberLibrary) {
-        System.out.println("MemberController.register");
-        System.out.println("User input info (ID: " + memberId + ", PW: " + memberPassword + ", Name: " + memberName + ", Phone: " +
-                            memberPhone + ", Email: " + memberEmail + ", Library: " + memberLibrary + ")");
+    @PostMapping("/member/save")
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        System.out.println("MemberController.save");
+        System.out.println("memberDTO = " + memberDTO);
+        memberService.save(memberDTO);
+
         return "home";
     }
 }
